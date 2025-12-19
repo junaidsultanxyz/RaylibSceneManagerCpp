8e64598cf8ec0112b41784e6603822afcce2d713
junaid sultan <jsultanwork@gmail.com>
2025-12-20 01:07:44 +0500

    gitignore

--- build/src/Game/CMakeFiles/Game.dir/main.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-src/Game/CMakeFiles/Game.dir/main.cpp.obj: \
- C:\Users\ASUS\Documents\coding\Raylib\SceneManagementCpp\src\Game\main.cpp \
- C:\Users\ASUS\Documents\coding\Raylib\SceneManagementCpp\src\Game\game/Game.h
